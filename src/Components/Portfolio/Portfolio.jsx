fb63a4d0285329427a5369430618819f5fe1f847
Foad <fwad40901@gmail.com>
2024-05-09 19:55:35 +0330

    Fix Images

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -6,42 +6,42 @@ export default function Portfolio(props) {
   const [category, SetCategory] = useState('all');
   const InfoCards = [
     {
-      img: '../../public/Images/Portfolio/portfolio-1.jpg',
+      img: 'Images/Portfolio/portfolio-1.jpg',
       titleBox: 'اینترنت 3',
       subtitle: 'اینترنت',
       category: 'Internet',
       id: 1,
     },
     {
-      img: '../../public/Images/Portfolio/portfolio-2.jpg',
+      img: 'Images/Portfolio/portfolio-2.jpg',
       titleBox: 'محصول 1',
       subtitle: 'محصول',
       category: 'Product',
       id: 2,
     },
     {
-      img: '../../public/Images/Portfolio/portfolio-3.jpg',
+      img: 'Images/Portfolio/portfolio-3.jpg',
       titleBox: 'محصول 3',
       subtitle: 'محصول',
       category: 'Product',
       id: 3,
     },
     {
-      img: '../../public/Images/Portfolio/portfolio-4.jpg',
+      img: 'Images/Portfolio/portfolio-4.jpg',
       titleBox: 'اینترنت 2',
       subtitle: 'اینترنت',
       category: 'Internet',
       id: 4,
     },
     {
-      img: '../../public/Images/Portfolio/portfolio-5.jpg',
+      img: 'Images/Portfolio/portfolio-5.jpg',
       titleBox: 'برنامه 3',
       subtitle: 'برنامه',
       category: 'Program',
       id: 5,
     },
     {
-      img: '../../public/Images/Portfolio/portfolio-6.jpg',
+      img: 'Images/Portfolio/portfolio-6.jpg',
       titleBox: 'برنامه 1',
       subtitle: 'برنامه',
       category: 'Program',
