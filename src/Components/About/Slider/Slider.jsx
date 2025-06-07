a440b98bec37cafb5cdbeabf32c20febc4b3a65c
Foad <fwad40901@gmail.com>
2025-06-08 03:24:32 +0330

    replace swiper slider

--- src/Components/About/Slider/Slider.jsx
+++ src/Components/About/Slider/Slider.jsx
@@ -1,128 +1,40 @@
 import React from 'react';
-import { Splide, SplideSlide } from '@splidejs/react-splide';
-import '@splidejs/react-splide/css/skyblue';
-import slider1 from '../../../../public/images/Slider/img1.png';
-import slider2 from '../../../../public/images/Slider/img2.png';
-import slider3 from '../../../../public/images/Slider/img3.png';
-import slider4 from '../../../../public/images/Slider/img4.png';
-import slider5 from '../../../../public/images/Slider/img5.png';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+
+import { adsSliderImg } from '../../../constants';
+
 export default function Slider() {
   return (
-    <Splide
-      className="items-center"
-      aria-label="My Favorite Images"
-      options={{
-        loop: true,
-        perPage: 5,
-        focus: 'center',
-        autoplay: true,
-        direction: 'rtl',
-        Pagination: false,
-        arrows: false,
-        breakpoints: {
-          500: {
-            perPage: 2,
+    <>
+      <Swiper
+        dir="rtl"
+        className="items-center px-2 my-3 lg:py-6"
+        spaceBetween={50}
+        slidesPerView={3}
+        breakpoints={{
+          768: {
+            slidesPerView: 4,
           },
-          700: {
-            perPage: 3,
-          },
-          800: {
-            perPage: 4,
-          },
-          1200: {
-            perPage: 6,
-          },
-          1300: {
-            perPage: 7,
-          },
-        },
-        // paginationDirection: 'rtl',
-      }}
-    >
-      <SplideSlide>
-        <img
-          src={slider1}
-          className="w-32 transition-all grayscale-[100] hover:grayscale-0"
-          alt=""
-        />
-      </SplideSlide>
-      <SplideSlide>
-        <img
-          src={slider2}
-          className="w-32 transition-all grayscale-[100] hover:grayscale-0"
-          alt="Image 2"
-        />
-      </SplideSlide>
-      <SplideSlide>
-        <img
-          src={slider3}
-          className="w-32 transition-all grayscale-[100] hover:grayscale-0"
-          alt=""
-        />
-      </SplideSlide>
-      <SplideSlide>
-        <img
-          src={slider4}
-          className="w-32 transition-all grayscale-[100] hover:grayscale-0"
-          alt="Image 2"
-        />
-      </SplideSlide>
-      <SplideSlide>
-        <img
-          src={slider5}
-          className="w-32 transition-all grayscale-[100] hover:grayscale-0"
-          alt="Image 2"
-        />
-      </SplideSlide>
-      <SplideSlide>
-        <img
-          src={slider1}
-          className="w-32 transition-all grayscale-[100] hover:grayscale-0"
-          alt=""
-        />
-      </SplideSlide>
-      <SplideSlide>
-        <img
-          src={slider2}
-          className="w-32 transition-all grayscale-[100] hover:grayscale-0"
-          alt="Image 2"
-        />
-      </SplideSlide>
-      <SplideSlide>
-        <img
-          src={slider3}
-          className="w-32 transition-all grayscale-[100] hover:grayscale-0"
-          alt=""
-        />
-      </SplideSlide>
-      <SplideSlide>
-        <img
-          src={slider4}
-          className="w-32 transition-all grayscale-[100] hover:grayscale-0"
-          alt="Image 2"
-        />
-      </SplideSlide>
-      <SplideSlide>
-        <img
-          src={slider5}
-          className="w-32 transition-all grayscale-[100] hover:grayscale-0"
-          alt="Image 2"
-        />
-      </SplideSlide>
-      <SplideSlide>
-        <img
-          src={slider1}
-          className="w-32 transition-all grayscale-[100] hover:grayscale-0"
-          alt="Image 2"
-        />
-      </SplideSlide>
-      <SplideSlide>
-        <img
-          src={slider2}
-          className="w-32 transition-all grayscale-[100] hover:grayscale-0"
-          alt="Image 2"
-        />
-      </SplideSlide>
-    </Splide>
+        }}
+        autoplay={{
+          delay: 2500,
+        }}
+        loop
+        modules={[Autoplay]}
+      >
+        {adsSliderImg.map(({ img }, index) => (
+          <SwiperSlide key={index + 1}>
+            <img
+              src={img}
+              className="w-20 md:w-28 transition-all grayscale-[100] hover:grayscale-0"
+              alt={`img slider ${index + 1}`}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
   );
 }
