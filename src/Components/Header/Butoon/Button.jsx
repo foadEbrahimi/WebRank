5c975c79417ed0647d3876e61010363720c61753
Foad <fwad40901@gmail.com>
2025-06-08 02:06:36 +0330

    fix button components in Header

--- src/Components/Header/Butoon/Button.jsx
+++ src/Components/Header/Butoon/Button.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 export default function Button() {
   return (
-    <button className="text-white  text-xl rounded-md px-5 py-1 border-2 border-Primary hover:bg-Primary transition-colors md:order-2">
+    <button className="text-white hidden md:flex text-xl rounded-md px-5 py-1 border-2 border-Primary hover:bg-Primary transition-colors md:order-2">
       شروع
     </button>
   );
