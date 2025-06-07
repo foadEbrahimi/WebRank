2d4aca7fe80b95edd49f2acceb6761b933a2c56d
Foad <fwad40901@gmail.com>
2025-06-08 02:06:24 +0330

    delete dynamic import in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React, { Suspense } from 'react';
-const About = React.lazy(() => import('./Components/About/About'));
-const Contact = React.lazy(() => import('./Components/Contact/Contact'));
-const Feedback = React.lazy(() => import('./Components/Feedback/Feedback'));
-const Footer = React.lazy(() => import('./Components/Footer/Footer'));
-const Header = React.lazy(() => import('./Components/Header/Header'));
-const Hero = React.lazy(() => import('./Components/Header/Hero/Hero'));
-const Portfolio = React.lazy(() => import('./Components/Portfolio/Portfolio'));
-const Servant = React.lazy(() => import('./Components/Servant/Servant'));
-const Team = React.lazy(() => import('./Components/Team/Team'));
+import About from './Components/About/About';
+import Contact from './Components/Contact/Contact';
+import Feedback from './Components/Feedback/Feedback';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+import Hero from './Components/Header/Hero/Hero';
+import Portfolio from './Components/Portfolio/Portfolio';
+import Servant from './Components/Servant/Servant';
+import Team from './Components/Team/Team';
+
 import './index.css';
 const titleSections = [
   { ID: 1, subTitle: 'خدمات', Title: 'خدمات ما را بررسی کنید' },
@@ -17,25 +17,23 @@ const titleSections = [
 ];
 function App() {
   return (
-    <Suspense fallback={<p>Loading ...</p>}>
-      <div className="font-Tanha">
-        <div className="App md:h-[100vb] flex flex-col gap-y-32 pb-4">
-          <Header />
-          <Hero />
-        </div>
-        <main className="space-y-32">
-          <About />
-          <Servant {...titleSections[0]} />
-          <Portfolio {...titleSections[1]} />
-          <Feedback />
-          <Team {...titleSections[2]} />
-          <Contact {...titleSections[3]} />
-        </main>
-        <footer className="mt-24">
-          <Footer />
-        </footer>
+    <div className="font-Tanha">
+      <div className="App md:h-[100vb] flex flex-col gap-y-32">
+        <Header />
+        <Hero />
       </div>
-    </Suspense>
+      <main className="space-y-32">
+        <About />
+        <Servant {...titleSections[0]} />
+        <Portfolio {...titleSections[1]} />
+        <Feedback />
+        <Team {...titleSections[2]} />
+        <Contact {...titleSections[3]} />
+      </main>
+      <footer className="mt-24">
+        <Footer />
+      </footer>
+    </div>
   );
 }
 
