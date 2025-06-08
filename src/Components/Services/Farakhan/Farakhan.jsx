1437b34689edfe98d9afdb55f8a9bb3b8b92ae46
Foad <fwad40901@gmail.com>
2025-06-08 03:49:03 +0330

    change some style in Farakhan component

---
+++ src/Components/Services/Farakhan/Farakhan.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Farakhan() {
+  return (
+    <div className="Farakhan text-center py-24 px-3 md:px-8 lg:px-12 xl:px-16 2xl:px-24 space-y-7">
+      <h1 className="text-white text-3xl font-bold">فراخوانی برای اقدام</h1>
+      <p className="text-white font-bold text-center leading-10">
+        اما محکوم کردن درد در فیلم غیرقابل تصور است ، در لذت می خواهد از درد
+        سیلوس بودن بدون نتیجه فرار کند. آنها استثنائاتی هستند که کورها به دنبال
+        آن هستند ، آنها نمی بینند ، آنها کسانی هستند که مسئولیت های خود را به
+        تقصیری واگذار می کنند که سختی های روح را تسکین می دهد.
+      </p>
+      <button className="border-2 border-white  bg-transparent text-white text-xl px-6 py-2 rounded-md hover:bg-Primary hover:text-white hover:border-Primary transition-all duration-300">
+        فراخوانی برای اقدام
+      </button>
+    </div>
+  );
+}
