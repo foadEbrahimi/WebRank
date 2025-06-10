a606eafee77508db4d794fed322461a561756032
Foad <fwad40901@gmail.com>
2025-06-11 00:41:43 +0330

    clear Portfolio section

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,53 +1,13 @@
 import React, { useState } from 'react';
+
 import Title from '../SectionTitles/Title';
 import Card from './Cards/Card';
 import Fittest from './Fittest/Fittest';
+import { portfolioInfo } from '../../constants';
+
 export default function Portfolio(props) {
   const [category, SetCategory] = useState('all');
-  const InfoCards = [
-    {
-      img: 'images/Portfolio/portfolio-1.jpg',
-      titleBox: 'اینترنت 3',
-      subtitle: 'اینترنت',
-      category: 'Internet',
-      id: 1,
-    },
-    {
-      img: 'images/Portfolio/portfolio-2.jpg',
-      titleBox: 'محصول 1',
-      subtitle: 'محصول',
-      category: 'Product',
-      id: 2,
-    },
-    {
-      img: 'images/Portfolio/portfolio-3.jpg',
-      titleBox: 'محصول 3',
-      subtitle: 'محصول',
-      category: 'Product',
-      id: 3,
-    },
-    {
-      img: 'images/Portfolio/portfolio-4.jpg',
-      titleBox: 'اینترنت 2',
-      subtitle: 'اینترنت',
-      category: 'Internet',
-      id: 4,
-    },
-    {
-      img: 'images/Portfolio/portfolio-5.jpg',
-      titleBox: 'برنامه 3',
-      subtitle: 'برنامه',
-      category: 'Program',
-      id: 5,
-    },
-    {
-      img: 'images/Portfolio/portfolio-6.jpg',
-      titleBox: 'برنامه 1',
-      subtitle: 'برنامه',
-      category: 'Program',
-      id: 6,
-    },
-  ];
+
   const categoryFun = category => {
     [...category.target.parentNode.childNodes].forEach(li =>
       li.classList.remove('active_category')
@@ -56,6 +16,7 @@ export default function Portfolio(props) {
     SetCategory((category = category.target.getAttribute('value')));
     console.log(category);
   };
+
   return (
     <div>
       <div className="space-y-12">
@@ -98,21 +59,21 @@ export default function Portfolio(props) {
             </div>
             <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
               {category === 'all' &&
-                InfoCards.filter(card => card.category !== 'all').map(card => (
-                  <Card {...card} key={card.id} />
-                ))}
+                portfolioInfo
+                  .filter(card => card.category !== 'all')
+                  .map(card => <Card {...card} key={card.id} />)}
               {category === 'Program' &&
-                InfoCards.filter(card => card.category === 'Program').map(
-                  card => <Card {...card} key={card.id} />
-                )}
+                portfolioInfo
+                  .filter(card => card.category === 'Program')
+                  .map(card => <Card {...card} key={card.id} />)}
               {category === 'Product' &&
-                InfoCards.filter(card => card.category === 'Product').map(
-                  card => <Card {...card} key={card.id} />
-                )}
+                portfolioInfo
+                  .filter(card => card.category === 'Product')
+                  .map(card => <Card {...card} key={card.id} />)}
               {category === 'Internet' &&
-                InfoCards.filter(card => card.category === 'Internet').map(
-                  card => <Card {...card} key={card.id} />
-                )}
+                portfolioInfo
+                  .filter(card => card.category === 'Internet')
+                  .map(card => <Card {...card} key={card.id} />)}
             </div>
           </div>
           <div>
