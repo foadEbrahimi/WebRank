db2c33f649b4bc3a933d9e532896b3bf95cd154e
Foad <fwad40901@gmail.com>
2025-06-12 17:39:23 +0330

    cleanup Team section

--- src/Components/Team/Card/Teams.jsx
+++ src/Components/Team/Card/Teams.jsx
@@ -1,30 +1,32 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
+
 import {
   BiLogoInstagramAlt,
   BiLogoFacebookCircle,
   BiLogoTwitter,
   BiLogoLinkedin,
-} from "react-icons/bi";
+} from 'react-icons/bi';
+
 export default function (data) {
-  const [social, setSocial] = useState([
+  const [social] = useState([
     <BiLogoFacebookCircle />,
     <BiLogoTwitter />,
     <BiLogoLinkedin />,
     <BiLogoInstagramAlt />,
   ]);
   return (
-    <div className="group shadow-lg shadow-gray-300 rounded-md w-fit">
+    <div className="group shadow-sm shadow-gray-300 rounded-xl w-fit">
       <div className="relative">
         <img
           src={data.src}
-          className="relative rounded-t-md w-[25rem] h-[25rem] md:w-[20rem] xl:w-[17rem] object-cover"
-          alt=""
+          className="relative rounded-t-lg w-[25rem] h-[20rem] md:w-[20rem] xl:w-[17rem] object-cover"
+          alt="member img"
         />
         <div className="Social opacity-0 group-hover:opacity-100 transition-all duration-500 flex items-center gap-x-5 absolute bottom-6 right-20 md:right-10 lg:right-12 xl:right-6 z-[2]">
           {social.map((icon, inedx) => (
             <span
               key={inedx}
-              className="text-2xl text-gray-100 bg-gray-400 p-2 rounded-xl cursor-pointer hover:text-gray-900 transition-all duration-500"
+              className="text-2xl text-gray-100 bg-Primary p-2 rounded-xl cursor-pointer hover:text-gray-900 transition-all duration-500"
             >
               {icon}
             </span>
--- src/Components/Team/Team.jsx
+++ src/Components/Team/Team.jsx
@@ -1,44 +1,18 @@
 import React from 'react';
+
 import Title from '../SectionTitles/Title';
 import Teams from './Card/Teams';
-import member1 from '../../../public/images/Team/team1.jpg';
-import member2 from '../../../public/images/Team/team2.jpg';
-import member3 from '../../../public/images/Team/team3.jpg';
-import member4 from '../../../public/images/Team/team4.jpg';
+
+import { TeamInfo } from '../../constants/index';
+
 export default function Team(props) {
-  const Data = [
-    {
-      id: 1,
-      name: 'فواد ابراهیمی',
-      src: member1,
-      silence: 'مالک',
-    },
-    {
-      id: 2,
-      name: 'حسام احمدی',
-      src: member2,
-      silence: 'توسعه دهنده',
-    },
-    {
-      id: 3,
-      name: 'امیرعباس ابارقی',
-      src: member3,
-      silence: 'مدیر ارشد',
-    },
-    {
-      id: 4,
-      name: 'امیرمحمد جعفری',
-      src: member4,
-      silence: 'مدیرعامل',
-    },
-  ];
   return (
     <div className="px-5 md:px-10 xl:px-28 2xl:px-36 space-y-20">
       <div>
         <Title {...props} />
       </div>
       <div className="flex flex-col justify-center items-center gap-10 md:flex-row flex-wrap">
-        {Data.map(team => (
+        {TeamInfo.map(team => (
           <Teams {...team} key={team.id} />
         ))}
       </div>
